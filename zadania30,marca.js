d17d24702bbd088542e22458b14fedc535e4ff5d
KonradWS97 <konrad.buckowski@gmail.com>
2021-03-30 22:43:36 +0200

    zajecia z dnia 30 marca

---
+++ zadania30,marca.js
@@ -0,0 +1,63 @@
+// let zip = document.getElementById("zip")
+// let wartosc = document.getElementById("wielkosc")
+
+// zip.addEventListener("input", function(val)
+// {   let wartoscZip =(zip.value)
+//     console.log(zip.value)
+//     wartosc.style.width=wartoscZip +'px'
+//     console.log(wartosc.style.width)
+//     wartosc.style.height = wartoscZip +'px'
+//     console.log(wartosc.style.height)
+//     let newElement = document.createElement('li')
+//     let maketxt=document.createTextNode(wartoscZip)
+//     let nowaWartosc =document.getElementById('nowa')
+//     // nowaWartosc.appendChild(newElement).appendChild(maketxt)
+//     // newElement.removeChild('li')
+//     // maketxt.removeChild(wartoscZip)
+//     let nW =document.getElementById('wielkosc1')
+//     nW.innerHTML = zip.value
+
+   
+// })
+// <<<<<<<<<<<<<<<<<zad_2>>>>>>>>>>>>>>>>>>>>>>>>
+// let Tabl= document.getElementById("tablica")
+// let tab=[5,12,17,23,123,45,54]
+// let dlugosc= tab.length
+// // let elementy = 
+// let wyswietlDane = ""
+// for(element of tab)
+// {
+// console.log(element)
+// wyswietlDane = wyswietlDane + element +", "
+// console.log(wyswietlDane)
+
+// }
+// console.log(wyswietlDane)
+// Tabl.innerHTML=wyswietlDane
+// <<<<<<<<<<<<<<<<<zad_3>>>>>>>>>>>>>>>>>>>>>>>>
+
+// let Tabl= document.getElementById("tablica")
+// let dl= document.getElementById("dlugosc")
+// let tab=[5,12,17,23,123,45,54]
+// let tabB =[]
+
+// // let elementy = 
+// let wyswietlDane = ""
+
+// tab .unshift(77)
+// tab .unshift(7)
+// tab.shift()
+// // let tabAll = tabB + tab
+
+// // console.log(tabAll)
+// let dlugosc= tab.length
+// // for(element of tabAll)
+// // {
+// // console.log(element)
+// // wyswietlDane = wyswietlDane + element +", "
+// // console.log(wyswietlDane)
+
+// // }
+// console.log(wyswietlDane)
+// Tabl.innerHTML=tab
+// dl.innerHTML=dlugosc
